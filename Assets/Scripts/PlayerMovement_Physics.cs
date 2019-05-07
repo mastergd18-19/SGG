2b62bca5e2ef4039116237b2fa6c0f13e2af0ed7
sergio9 <sergiogomez91@gmail.com>
2019-05-07 21:43:07 +0200

    Cambios clase

---
+++ Assets/Scripts/PlayerMovement_Physics.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_Physics : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float acceleration;
+    public float deceleration;
+    public bool moving;
+
+
+    private Rigidbody rb;
+    public float rR=1;
+    public float t=1;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        InvokeRepeating("CheckSpeed", t, rR);
+    }
+
+    void Movement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * acceleration);
+
+        if (Input.GetAxis("Horizontal") != 0.0f | Input.GetAxis("Vertical") != 0.0f)
+        {
+            moving = true;
+        }
+        else
+        {
+            moving = false;
+        }
+    }
+
+    void MovementDeceleration()
+    {
+        if (moving == false)
+        {
+            rb.angularDrag = deceleration;
+        }
+        else
+        {
+            rb.angularDrag = 0.05f;
+        }
+    }
+
+    void Update()
+    {
+        Movement();
+        MovementDeceleration();
+    }
+
+    void CheckSpeed()
+    {
+        Debug.Log("Velocidad en x: " + rb.velocity.x);
+        Debug.Log("Velocidad en y: " + rb.velocity.y);
+        Debug.Log("Velocidad en z: " + rb.velocity.z);
+    }
+ }
+
---
+++ Assets/Scripts/PlayerMovement_Translation.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_Translation : MonoBehaviour
+{
+    public float speed;
+    public bool moving;
+    public float rR = 1;
+    public float t = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Movement", t, rR);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Movement()
+    {
+
+        transform.position += Vector3.back;
+
+
+
+        //float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        //float moveVertical = Input.GetAxisRaw("Vertical");
+
+        //if (Input.GetKey(KeyCode.W))
+        //{
+
+        //}
+
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.Translate(Vector3.back * speed * Time.deltaTime);
+        //}
+
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.Translate(Vector3.right * speed * Time.deltaTime);
+        //}
+
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.Translate(Vector3.left * speed * Time.deltaTime);
+        //}
+
+    }
+}
